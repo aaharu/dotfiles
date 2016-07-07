6695923b101ec9f361b3ec507f07352f4e8b6331
aaharu <aaharu@hotmail.com>
2016-07-08 06:53:37 +0900

    fix pt

--- setupgo.sh
+++ setupgo.sh
@@ -4,5 +4,5 @@ if [ -x "$(which go)" ] ; then
     go get -u github.com/motemen/ghq
     go get -u github.com/peco/peco/cmd/peco
     go get -u github.com/github/hub
-    go get -u github.com/monochromegane/the_platinum_searcher
+    go get -u github.com/monochromegane/the_platinum_searcher/cmd/pt
 fi
